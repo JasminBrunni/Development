7d2775f766210a91fb8f7e36278d228cd7b11647
Jasmin Brunni <c0brja00@students.oamk.fi>
2021-09-17 04:23:31 +0300

    Alkometri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import testUtils from 'react-dom/test-utils';
 import './App.css';
 
 function App() {
+
+  const [paino, setWeight] = useState(0);
+  const [gender, setGender] = useState();
+  const [määrä, setPortions] = useState(0);
+  const [aika, setTime] = useState(0);
+  const [woman, resultW] = useState(0);
+  const [man, resultM] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    const annostenMäärä = määrä * 0.33
+    const poltto = paino / 10
+    const polttoJälkeen = paino - (poltto * aika)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Alkometri</h3>
+      <form>
+      <div>
+        <label>Sukupuoli</label>
+      </div>
+      <input type="sukupuoli"
+      value={gender}
+      onChange={e => setGender(e.target.value)}/>
+
+      <div>
+        <label>Paino (kg)</label>
+      </div>
+      <input type="paino"
+         value={paino}
+         onChange={e => setWeight(e.target.value)}/>
+
+      <div>
+        <label>Annosten määrä</label>
+      </div>
+      <input type="määrä"
+        value={määrä}
+        onChange={e => setPortions(e.target.value)}/>
+
+      <div>
+        <label>Aika</label>
+      </div>
+      <input type="aika"
+        value={aika}
+        onChange={e => setTime(e.target.value)}/>
+
+      <div>
+      <button>Laske</button>
+      <output>{resultW}</output>
+      <output>{resultM}</output>
+      </div>
+      </form>
+
+
     </div>
   );
 }
