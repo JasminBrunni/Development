3b0a0d656697f28d7d8a125ee187c3e5d03b69ac
Jasmin Brunni <c0brja00@students.oamk.fi>
2021-09-17 18:15:39 +0300

    lopullinen

--- src/App.js
+++ src/App.js
@@ -5,61 +5,120 @@ import './App.css';
 function App() {
 
   const [paino, setWeight] = useState(0);
-  const [gender, setGender] = useState();
-  const [määrä, setPortions] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [pullot, setPortions] = useState(0);
   const [aika, setTime] = useState(0);
-  const [woman, resultW] = useState(0);
-  const [man, resultM] = useState(0);
+  const [tulos, setResult] = useState(0);
 
-  function calculate(e) {
+
+  function handleSubmit(e) {
     e.preventDefault();
-    const annostenMäärä = määrä * 0.33
-    const poltto = paino / 10
-    const polttoJälkeen = paino - (poltto * aika)
+
+    const litrat = pullot * 0.33;
+    const grammat = litrat * 8 * 4.5;
+    const polttaa = paino / 10;
+    const jäljelläGrammat = grammat - polttaa * aika;
+    let alkoHolipitoisuus = 0;
+    if (gender === 'male') {
+       alkoHolipitoisuus = jäljelläGrammat / (paino * 0.7);
+    } else {
+      alkoHolipitoisuus = jäljelläGrammat / (paino * 0.6);
+    }
+
+    if (alkoHolipitoisuus < 0 || isNaN(alkoHolipitoisuus)) {
+      setResult(0);
+    } else {
+      setResult(alkoHolipitoisuus);
+    }
 
   }
 
   return (
     <div id="container">
       <h3>Alkometri</h3>
-      <form>
-      <div>
-        <label>Sukupuoli</label>
-      </div>
-      <input type="sukupuoli"
-      value={gender}
-      onChange={e => setGender(e.target.value)}/>
+      <form onSubmit={handleSubmit}>
 
       <div>
         <label>Paino (kg)</label>
-      </div>
-      <input type="paino"
+      
+      <input type="paino" type="number" min="1"
          value={paino}
-         onChange={e => setWeight(e.target.value)}/>
-
+         onChange={(e) => setWeight(e.target.value)}/>
+      </div>
       <div>
         <label>Annosten määrä</label>
-      </div>
-      <input type="määrä"
-        value={määrä}
-        onChange={e => setPortions(e.target.value)}/>
+      <select type="number" name="pullot"
+        value={pullot}
+        onChange={(e) => setPortions(e.target.value)}>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="7">14</option>
+          <option value="7">15</option>
+        </select>
+        </div>
 
       <div>
         <label>Aika</label>
-      </div>
-      <input type="aika"
+      <select type="aika"
         value={aika}
-        onChange={e => setTime(e.target.value)}/>
+        onChange={(e) => setTime(e.target.value)}>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="7">14</option>
+          <option value="7">15</option>
+          <option value="1">16</option>
+          <option value="2">17</option>
+          <option value="3">18</option>
+          <option value="4">19</option>
+          <option value="5">20</option>
+          <option value="6">21</option>
+          <option value="7">22</option>
+          <option value="8">23</option>
+          <option value="9">24</option>
+      </select>
+      </div>
+      <div>
+        <label>Sukupuoli</label>
+        <input type="radio" name="sukupuoli" value="male" defaultChecked onChange={(e) => setGender(e.target.value)}/>
+        <label>Mies</label>
+        <input type="radio" name="sukupuoli" value="female" onChange={(e) => setGender(e.target.value)}/>
+        <label>Nainen</label>
+      </div>
 
       <div>
-      <button>Laske</button>
-      <output>{resultW}</output>
-      <output>{resultM}</output>
+        <output>{tulos.toFixed(1)}</output>
       </div>
-      </form>
 
-
+      <button>Laske</button>
+      
+      
+    </form>
     </div>
+    
   );
 }
 
